879225a8d1340eeead0be425137dd4a17f4a6b7c
Carlos Valderrama <81756261+ascxiao@users.noreply.github.com>
2026-01-03 22:47:36 +0800

    added some more

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$title}}</title>
+
+    <link rel="icon" href="{{ asset('personal-logo.svg') }}" type="image/x-icon">
     
     <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
     @vite('resources/css/app.css')
